32f87e16b67c1765b98ab2304b195e2cff624081
alvaro20182019 <39622789+alvaro20182019@users.noreply.github.com>
2018-06-21 12:12:43 +0100

    criado o viewHolder do livro

--- app/src/main/java/pt/ipg/registarlivros/BooksCursorAdapter.java
+++ app/src/main/java/pt/ipg/registarlivros/BooksCursorAdapter.java
@@ -1,8 +1,10 @@
 package pt.ipg.registarlivros;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,12 +13,19 @@ import android.view.ViewGroup;
  */
 
 public class BooksCursorAdapter extends RecyclerView.Adapter<BooksCursorAdapter.bookViewHolder> {
+    private  Context context;
 
+    private Cursor cursor=null;
 
+    public BooksCursorAdapter(Context context){
+        this.context=context;
+    }
     @NonNull
     @Override
+
     public bookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View item = LayoutInflater.from(context).inflate(R.layout.item_book,parent,false);
+        return new bookViewHolder(item);
     }
 
     @Override
